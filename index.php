8a8765655354d4891ff63206a8da1a23bf6d2c28
FrankSinatra1 <smarttv201404@gmail.com>
2019-06-23 14:55:58 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Hackaton</title>
+	<script
+  src="https://code.jquery.com/jquery-3.3.1.min.js"
+  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+	crossorigin="anonymous"></script>
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,700" rel="stylesheet">
+	<link rel="stylesheet" href="css/main.css">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+	rel="stylesheet">
+</head>
+<body>
+	<div class="wrapper">
+		<div class="block-shadow">
+			<p>Здравствуйте!<br>Введите свой номер телефона</p>
+		</div>
+		<div class="block-shadow">
+			<form action="">
+				<input type="text" id="phone">
+			</form>
+		</div>
+		<div class="button">
+			Подтвердить
+		</div>
+		<img src="img/back.png" alt="" class="background">
+	</div>
+	<script src="js/jquery.maskedinput.min.js"></script>
+	<script src="js/main.js"></script>
+</body>
+</html>
